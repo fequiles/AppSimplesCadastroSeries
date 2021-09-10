145047ec4b48675dfc045b59fa182c1f078a4664
fequiles <feliperibeiro173@gmail.com>
2021-09-10 16:42:42 -0300

    Arquivos de enums e interfaces

---
+++ Enum/Genero.cs
@@ -0,0 +1,12 @@
+namespace DIO.Series
+{
+    public enum Genero 
+    {
+        Acao = 1,
+        Aventura = 2,
+        Terror = 3,
+        Animacao = 4,
+        Fantasia = 5,
+        Suspense = 6
+    }
+}
